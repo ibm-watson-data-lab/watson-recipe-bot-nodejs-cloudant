6dda1d109064c329d53fe933b545cf9b1e66330a
Mark Watson <mark.watson.x@gmail.com>
2016-12-14 23:04:25 -0600

    Clean up

--- WebSocketBot.js
+++ WebSocketBot.js
@@ -20,13 +20,13 @@ class WebSocketBot extends EventEmitter {
     }
 
     onWebSocketConnection(request) {
-        console.log(new Date() + ' WebSocket connection accepted.');
+        console.log(`${new Date()} WebSocket connection accepted.`);
         let connection = request.accept(null, request.origin);
         let client = new WebSocketClient(connection);
         this.clients.push(client);
         connection.on('message', (message) => {
             if (message.type === 'utf8') {
-                console.log(new Date() + ' WebSocket server received message: ' + message.utf8Data);
+                console.log(`${new Date()} WebSocket server received message: ${message.utf8Data}`);
                 let data = JSON.parse(message.utf8Data);
                 this.onMessageReceivedFromClient(client, data);
             }
@@ -35,7 +35,7 @@ class WebSocketBot extends EventEmitter {
             let index = this.clients.indexOf(client);
             if (index >=0 ) {
                 this.clients.splice(index, 1);
-                console.log(new Date() + ' WebSocket client ' + connection.remoteAddress + ' disconnected.');
+                console.log(`${new Date()} WebSocket client ${connection.remoteAddress} disconnected.`);
             }
         });
     }
